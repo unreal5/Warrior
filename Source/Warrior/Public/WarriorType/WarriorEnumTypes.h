cd9763b762f63d71fbc4fceaac4751fadbfd2350
unreal5 <lingbao@outlook.com>
2024-08-13 18:03:42 +0800

    64 on target interacted

---
+++ Source/Warrior/Public/WarriorType/WarriorEnumTypes.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "WarriorEnumTypes.generated.h"
+
+UENUM()
+enum class EWarriorConfirmType : uint8
+{
+	Yes,
+	No,
+};
+
+UENUM()
+enum class EWarriorValidType: uint8
+{
+	Valid,
+	Invalid,
+};
+
+UENUM(BlueprintType)
+enum class EToggleDamageType : uint8
+{
+	CurrentEquippedWeapon,
+	LeftHand,
+	RightHand,
+};
