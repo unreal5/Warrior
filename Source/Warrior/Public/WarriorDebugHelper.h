46f2faf6e44e51197ac5fdffebda593ea394fa64
unreal5 <lingbao@outlook.com>
2024-08-14 17:00:19 +0800

    74 modify health attribute

--- Source/Warrior/Public/WarriorDebugHelper.h
+++ Source/Warrior/Public/WarriorDebugHelper.h
@@ -10,4 +10,15 @@ namespace Debug
 		}
 		UE_LOG(LogTemp, Warning, TEXT("%s"), *Message);
 	}
+
+	static void Print(const FString& FloatTitle, float FloatValueToPrint, int32 InKey = -1,
+	                  const FColor& Color = FColor::MakeRandomColor())
+	{
+		const FString Message = FloatTitle + TEXT(": ") + FString::SanitizeFloat(FloatValueToPrint);
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(InKey, 5.f, Color, Message);
+		}
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *Message);
+	}
 }
