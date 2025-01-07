e9414c3b83c857296bbbc2f6c68870d9c748c8d7
lbjtj <lbjtj@outlook.com>
2025-01-07 17:41:48 +0800

    0061 pawn combat interface 16:32

---
+++ Source/Warrior/Public/WarriorType/WarriorEnumTypes.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "WarriorEnumTypes.generated.h"
+
+UENUM()
+enum class EWarriorConfirmType : uint8
+{
+	Yes,
+	No,
+};
+
+UENUM()
+enum class EWarriorValidType : uint8
+{
+	Valid,
+	Invalid,
+};
