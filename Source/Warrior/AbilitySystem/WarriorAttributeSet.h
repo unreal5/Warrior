666ebc0330ac0429e45fe24495948263d6f0b03e
lbjtj <lbjtj@outlook.com>
2025-07-23 17:28:32 +0800

    0058. Gameplay Effect

--- Source/Warrior/AbilitySystem/WarriorAttributeSet.h
+++ Source/Warrior/AbilitySystem/WarriorAttributeSet.h
@@ -3,14 +3,42 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
 #include "AttributeSet.h"
 #include "WarriorAttributeSet.generated.h"
 
 /**
- * 
+ * ATTRIBUTE_ACCESSORS_BASIC 代替ATTRIBUTE_ACCESSORS
  */
 UCLASS()
 class WARRIOR_API UWarriorAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+
+public:
+	UWarriorAttributeSet();
+
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+	FGameplayAttributeData CurrentHealth;
+	ATTRIBUTE_ACCESSORS_BASIC(ThisClass, CurrentHealth);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Health")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS_BASIC(ThisClass, MaxHealth);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Rage")
+	FGameplayAttributeData CurrentRage;
+	ATTRIBUTE_ACCESSORS_BASIC(ThisClass, CurrentRage);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Rage")
+	FGameplayAttributeData MaxRage;
+	ATTRIBUTE_ACCESSORS_BASIC(ThisClass, MaxRage);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Damage")
+	FGameplayAttributeData AttackPower;
+	ATTRIBUTE_ACCESSORS_BASIC(ThisClass, AttackPower);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Damage")
+	FGameplayAttributeData DefensePower;
+	ATTRIBUTE_ACCESSORS_BASIC(ThisClass, DefensePower);
 };
